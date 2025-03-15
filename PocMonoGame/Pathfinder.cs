8499ef4d2bf0f2bea73083c71b7b10c1ae83ba4c
Servan42 <charlot.servan@gmail.com>
2025-03-15 22:29:07 +0100

    re:f Extracted pathfinder

--- PocMonoGame/Pathfinder.cs
+++ PocMonoGame/Pathfinder.cs
@@ -1,12 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace PocMonoGame
 {
     internal class Pathfinder
     {
+        public IEnumerable<(int x, int y)> GeneratePathAstar((int x, int y) firstNode, (int x, int y) destinationNode, int boardWitdh, int boardHeight, Func<(int x, int y), bool> isObstacle)
+        {
+            PriorityQueue<(int x, int y), int> fronteir = new PriorityQueue<(int x, int y), int>();
+            fronteir.Enqueue(firstNode, 0);
+
+            Dictionary<(int x, int y), (int x, int y)> keyCameFromValue = new();
+            keyCameFromValue.Add(firstNode, firstNode);
+
+            Dictionary<(int x, int y), int> costSoFar = new();
+            costSoFar.Add(firstNode, 0);
+
+            while (fronteir.Count > 0)
+            {
+                var currentNode = fronteir.Dequeue();
+                if (currentNode == destinationNode)
+                    break;
+
+                foreach (var direction in new (int alongX, int alongY)[] { (1, 0), (-1, 0), (0, 1), (0, -1) })
+                {
+                    (int x, int y) neighboor = (currentNode.x + direction.alongX, currentNode.y + direction.alongY);
+                    
+                    if (IsOutOfBounds(boardWitdh, boardHeight, neighboor))
+                        continue;
+
+                    if (isObstacle(neighboor))
+                        continue;
+
+                    var newCost = costSoFar[currentNode] + 1;
+                    if (keyCameFromValue.ContainsKey(neighboor) && newCost >= costSoFar[neighboor])
+                        continue;
+
+                    if (costSoFar.ContainsKey(neighboor))
+                    {
+                        keyCameFromValue[neighboor] = currentNode;
+                        costSoFar[neighboor] = newCost;
+                    }
+                    else
+                    {
+                        keyCameFromValue.Add(neighboor, currentNode);
+                        costSoFar.Add(neighboor, newCost);
+                    }
+
+                    var priority = newCost + GetHeuristicDistanceToGoal(neighboor, destinationNode);
+                    fronteir.Enqueue(neighboor, priority);
+                }
+            }
+
+            if (!keyCameFromValue.ContainsKey(destinationNode))
+                return [];
+
+            return BuildPath(keyCameFromValue, firstNode, destinationNode);
+        }
+
+        private bool IsOutOfBounds(int boardWitdh, int boardHeight, (int x, int y) neighboor)
+        {
+            return neighboor.x < 0 || neighboor.x > boardWitdh || neighboor.y < 0 || neighboor.y > boardHeight;
+        }
+
+        private IEnumerable<(int x, int y)> BuildPath(Dictionary<(int x, int y), (int x, int y)> keyCameFromValue, (int x, int y) startNode, (int x, int y) destinationNode)
+        {
+            var path = new List<(int x, int y)>();
+            var current = destinationNode;
+            while (current != startNode)
+            {
+                path.Add(current);
+                current = keyCameFromValue[current];
+            }
+            path.Add(startNode);
+            path.Reverse();
+            return path;
+        }
+
+        private int GetHeuristicDistanceToGoal((int x, int y) startNode, (int x, int y) destinationNode)
+        {
+            // Manhattan distance
+            return Math.Abs(destinationNode.x - startNode.x) + Math.Abs(destinationNode.y - startNode.y);
+        }
     }
 }
--- PocMonoGame/SnakeBoard.cs
+++ PocMonoGame/SnakeBoard.cs
@@ -48,7 +48,14 @@ namespace PocMonoGame
             if(path.Count == 0)
             {
                 food = GenerateRandomFoodPosition();
-                GenerateNextPath();
+                path = new Queue<(int x, int y)>(new Pathfinder().GeneratePathAstar(
+                    GetSnakeHeadPos(),
+                    food,
+                    width,
+                    height,
+                    snake.Contains
+                ));
+
                 if(path.Count == 0)
                 {
                     bestScore = Math.Max(bestScore, snake.Count - 1);
@@ -83,78 +90,6 @@ namespace PocMonoGame
                 food = (random.Next(0, width), random.Next(0, height));
             } while(snake.Contains(food));
             return food;
-        }
-
-        private void GenerateNextPath()
-        {
-            var firstNode = GetSnakeHeadPos();
-
-            PriorityQueue<(int x, int y), int> fronteir = new PriorityQueue<(int x, int y), int>();
-            fronteir.Enqueue(firstNode, 0);
-
-            Dictionary<(int x, int y), (int x, int y)> keyCameFromValue = new();
-            keyCameFromValue.Add(firstNode, firstNode);
-
-            Dictionary<(int x, int y), int> costSoFar = new();
-            costSoFar.Add(firstNode, 0);
-
-            while (fronteir.Count > 0)
-            {
-                var currentNode = fronteir.Dequeue();
-                if (currentNode == food)
-                    break;
-
-                foreach (var direction in new (int alongX, int alongY)[] { (1, 0), (-1, 0), (0, 1), (0, -1)})
-                {
-                    (int x, int y) neighboor = (currentNode.x + direction.alongX, currentNode.y + direction.alongY);
-                    if(neighboor.x < 0 || neighboor.x > width || neighboor.y < 0 || neighboor.y > height)
-                        continue;
-
-                    if (snake.Contains(neighboor))
-                        continue;
-
-                    var newCost = costSoFar[currentNode] + 1;
-                    if (keyCameFromValue.ContainsKey(neighboor) && newCost >= costSoFar[neighboor])
-                        continue;
-
-                    if (costSoFar.ContainsKey(neighboor))
-                    {
-                        keyCameFromValue[neighboor] = currentNode;
-                        costSoFar[neighboor] = newCost;
-                    }
-                    else
-                    {
-                        keyCameFromValue.Add(neighboor, currentNode);
-                        costSoFar.Add(neighboor, newCost);
-                    }
-
-                    var priority = newCost + GetHeuristicDistanceToGoal(neighboor);
-                    fronteir.Enqueue(neighboor, priority);
-                }
-            }
-
-            if (!keyCameFromValue.ContainsKey(food))
-                return;
-
-            BuildPath(keyCameFromValue, firstNode);
-        }
-
-        private void BuildPath(Dictionary<(int x, int y), (int x, int y)> keyCameFromValue, (int x, int y) startNode)
-        {
-            var current = food;
-            while (current != startNode)
-            {
-                path.Enqueue(current);
-                current = keyCameFromValue[current];
-            }
-
-            path.Enqueue(startNode);
-            path = new Queue<(int x, int y)>(path.Reverse());
-        }
-
-        private int GetHeuristicDistanceToGoal((int x, int y) pos)
-        {
-            return Math.Abs(food.x - pos.x) + Math.Abs(food.y - pos.y);
         }
     }
 }
